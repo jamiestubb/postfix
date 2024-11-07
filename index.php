483fcf891b7bcbaad68454c210e51749ebea844b
jamiestubb <163948058+jamiestubb@users.noreply.github.com>
2024-11-07 17:03:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dual CAPTCHA Verification</title>
+    <title>CAPTCHA Verification Gateway</title>
     <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
     <style>
         body,
